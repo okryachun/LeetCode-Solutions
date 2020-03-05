d3f187431afff67eac3b5ad018ba1ce4fdb6e3de
Oleg Kryachun <oleg.kryachun@wsu.edu>
2020-03-05 15:35:24 -0800

    Update subString.c

--- Longest Substring/subString.c
+++ Longest Substring/subString.c
@@ -1,24 +1,33 @@
+#include <stdbool.h>
+#include <stdio.h>
 
 int lengthOfLongestSubstring(char * s){
     int head = 0, tail = 0, x = 0, curlen = 0, maxlen = 0, pos = 0;
     bool present = false;
-    
-    while(s[head] != '\0') {
-        present = false; pos = 0;
-        for(x = tail; x < head; x++) {
+
+    while(s[head] != '\0') {  //loop through the list
+        present = false;    //reset the present flag each iteration
+        for(x = tail; x < head; x++) {  //loop from tail to char before head
+                                       //checking if new head exists between tail and old head
             if(s[x] == s[head]) {
-                present = true;
+                present = true;     //if head does exist, set the present flag
                 break;
             }
         }
         pos = x;
         if(present) {
-            maxlen = curlen > maxlen ? curlen : maxlen;
-            tail = pos + 1;
+            maxlen = curlen > maxlen ? curlen : maxlen; //update maxlen value
+            tail = pos + 1; //reset tail relative to when the duplicated character was found
         }
         head++;
-        curlen = head - tail; 
-        
-    }                
+        curlen = head - tail;   //update curlen
+    }
     return maxlen > curlen ? maxlen : curlen;
+}
+
+int main(int argc, char *argv[]) {
+
+    if(argc > 1)
+        printf("Longest substring: %d\n", lengthOfLongestSubstring(argv[1]));
+    return 0;
 }
